36dd54bc858fe2a4eb88ef0570b086d9a80cfa3b
grantbirki <grantbirki@github.com>
2023-05-22 22:05:42 +0100

    add featured blog post section

--- src/config/pages/home/featured-posts.js
+++ src/config/pages/home/featured-posts.js
@@ -2,24 +2,20 @@
 
 const featuredPosts = {
   // Whether the component is enabled or not
-  enabled: false,
+  enabled: true,
 
   // The title of the component
-  title: 'Find out more content in our Blog',
+  title: 'Find out more content in our EDF Blog',
 
   // The description of the component / information
-  information: `The blog is used to display AstroWind documentation.
-  Each new article will be an important step that you will need to know to be an expert in creating a website using Astro + Tailwind CSS.
-  Astro is a very interesting technology. Thanks.
+  information: `The EDF blog contains all sorts of information about the event, planning for future events, and memories from past events.
   `,
 
   // The list of posts to be displayed
   // These ids are the file names of the posts in the content/post folder
   postIds: [
-    'get-started-website-with-astro-tailwind-css.md',
-    'how-to-customize-astrowind-to-your-brand.md',
-    'useful-resources-to-create-websites.md',
-    'astrowind-template-in-depth.md',
+    'edf-2023.md',
+    'edf-2022-memories.md',
   ],
 };
 
